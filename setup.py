a58dd6b684f1cc938f9f6d6fc780b55a137d6aab
jenkins <jenkins@nalkins.cloud>
2018-09-07 14:26:04 +0300

    update version to: 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-user-email-extension",
-    version="0.1.4",
+    version="0.1.5",
     author="Arie Lev",
     author_email="levinson.arie@gmail.com",
     description="User model extender for django",

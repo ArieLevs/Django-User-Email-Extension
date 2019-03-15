50416dbfa0f34efa544bb4d8a2e3f44aa19b600b
jenkins <jenkins@nalkins.cloud>
2019-03-15 19:04:33 +0000

    update version to: 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-user-email-extension",
-    version="1.0.0",
+    version="1.0.1",
     author="Arie Lev",
     author_email="levinson.arie@gmail.com",
     description="User model extender for django",

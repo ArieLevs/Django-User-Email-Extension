bf57aecea5a0339f8ed1b97061a71274d7e4965c
arielev <levinsonarie@gmail.com>
2019-03-19 22:20:00 +0200

    Remove password requirement for social auth integration

--- django_user_email_extension/models.py
+++ django_user_email_extension/models.py
@@ -18,8 +18,6 @@ class UserManager(BaseUserManager):
         """
         if not email:
             raise ValueError('Email must be set')
-        if not password:
-            raise ValueError('Password must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)

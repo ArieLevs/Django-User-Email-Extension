cf2af061c7cde50f8bf1037c084506155cba4f3e
GitHub Action <ArieLevs@users.noreply.github.com>
2024-09-12 05:02:43 +0000

    update version to: 2.5.1 (github action)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-user-email-extension",
-    version="2.5.0",
+    version="2.5.1",
     author="Arie Lev",
     author_email="levinson.arie@gmail.com",
     description="User model extender for django",
